83900e2b42536bd59c1abb7a275bad01ddbbc064
d-murphy <murphy.dan.s@gmail.com>
2021-05-17 22:50:08 -0400

    added relationships between objects

--- ToDo.md
+++ ToDo.md
@@ -1,7 +1,22 @@
 # To Do List
 
-* Complete login page (left off when [posts](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database) were introduced)
 * Define items for database (users, courses, videos)
-
+* Add a unit test
 * add password for admin access
-* 
+
+________
+
+ORM: 
+
+One to one
+* users create videos  (Lesson.addedBy and User.lessonsCreated)
+* users create courses (Course.addedBy and User.coursesCreated)
+
+many to many
+* users join courses
+* users complete videos 
+* videos belong to courses
+
+
+Completed: 
+* Complete login page
--- app/models.py
+++ app/models.py
@@ -3,6 +3,24 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+usersCoursesAssociation = db.Table('usersCoursesAssociation', db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'))
+)
+
+usersLessonCompleteAssociation = db.Table('usersLessonCompleteAssociation', db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('lesson_id', db.Integer, db.ForeignKey('lesson.id')), 
+    db.Column('date_completed', db.DateTime, index=True, default=datetime.utcnow)
+
+)
+
+courseVideosAssociation = db.Table('courseVideosAssociation', db.Model.metadata,
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
+    db.Column('lesson_id', db.Integer, db.ForeignKey('lesson.id'))
+)
+
+
 class User(UserMixin, db.Model):
 
     def set_password(self, password):
@@ -11,28 +29,53 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def completeLesson(self, lesson):
+        self.lessonsComplete.append(lesson)
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username) 
+
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     adminPrivilege = db.Column(db.Boolean(), index=True)
     lessonsCreated = db.relationship('Lesson', backref='author', lazy='dynamic')
+    coursesCreated = db.relationship('Course', backref='author', lazy='dynamic')
+    coursesEnrolled = db.relationship('Course', secondary=usersCoursesAssociation, backref="courseStudents")
+    lessonsComplete = db.relationship('Lesson', secondary=usersLessonCompleteAssociation, backref="lessonStudents")
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username) 
+class Lesson(db.Model):
+
+    def getCompletedLessons(self):
+        query = Lesson.query.join(
+            usersLessonCompleteAssociation, (usersLessonCompleteAssociation.c.lesson_id == Lesson.id)
+            ).order_by(Lesson.dateAdded)
+        return query
+
+    __tablename__ = 'lesson'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=True)
+    duration = db.Column(db.Integer)
+    dateAdded = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    createdBy = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Course(db.Model):
+    __tablename__ = 'course'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True)
+    createdBy = db.Column(db.Integer, db.ForeignKey('user.id'))
+    lessonsIncluded = db.relationship('Lesson', secondary=courseVideosAssociation, backref="lessonInCourse")
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-class Lesson(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), index=True, unique=True)
-    duration = db.Column(db.Integer)
-    dateAdded = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    addedBy = db.Column(db.Integer, db.ForeignKey('user.id'))
+def getUserLessons(user_id):
+    print(db.session.query(usersLessonCompleteAssociation).join(Lesson).all())
 
-class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), index=True)
+def getUserLessons():
+    print(db.session.query(usersLessonCompleteAssociation.c.lesson_id).join(Lesson).all())
 
